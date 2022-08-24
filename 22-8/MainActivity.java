7f548ea6e0cebd2945321145df8cdca98a1df742
prakrutitops <109515514+prakrutitops@users.noreply.github.com>
2022-08-24 16:14:38 +0530

    Add files via upload

--- 22-8/MainActivity.java
+++ 22-8/MainActivity.java
@@ -1,26 +1,93 @@
-package com.tops.WakeLockExample;
+package com.example.sensorex;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.app.Activity;
-import android.content.Context;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-	
-	PowerManager pm;
-	PowerManager.WakeLock wl;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-		if (pm != null) {
-			wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WakeLockExample:rushabh");
-		}
+public class MainActivity extends AppCompatActivity implements SensorEventListener
+{
+    SensorManager sensorManager;
+    TextView textView;
+    private boolean color = false;
+    private long lastUpdate;
 
-		wl.acquire(10*60*1000L /*10 minutes*/);
-		//...
-		wl.release();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
+        textView=findViewById(R.id.txt1);
+        textView.setBackgroundColor(Color.GREEN);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent)
+    {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+            getAccelerometer(sensorEvent);
+        }
+    }
+
+    private void getAccelerometer(SensorEvent sensorEvent)
+    {
+
+        float[] values = sensorEvent.values;
+
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelationSquareRoot = (x * x + y * y + z * z) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        long actualTime = sensorEvent.timestamp;
+        if (accelationSquareRoot >= 2) //
+        {
+            if (actualTime - lastUpdate < 200)
+            {
+                return;
+            }
+
+            lastUpdate = actualTime;
+            Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT).show();
+
+            if (color)
+            {
+                textView.setBackgroundColor(Color.GREEN);
+            }
+            else
+                {
+                textView.setBackgroundColor(Color.RED);
+            }
+            color=!color;
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i)
+    {
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 }
